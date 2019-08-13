b4bbfdf6974d4ec96b1e2fa235241b009a22c846
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-08-13 18:19:28 -0500

    Attempt one at Part two

--- models/skill.js
+++ models/skill.js
@@ -1,18 +1,33 @@
 const skills = [
-    {skill: 'Front-End Languages', done: true},
-    {skill: 'Python Knowledge', done: false},
-    {skill: 'Ability to Develop Apps', done: false}
-  ];
+  {skill: 'Front-End Languages', done: true},
+  {skill: 'Python Knowledge', done: false},
+  {skill: 'Ability to Develop Apps', done: false}
+];
   
-  module.exports = {
-    getAll,
-    getOne
-  };
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne,
+  update
+};
 
-  function getOne(id) {
-    return skills[id];
-  }
-  
-  function getAll() {
-    return skills;
-  }
+function update(id, skill) {
+  skills[id] = skill;
+}
+
+function deleteOne(id) {
+  skills.splice(id, 1);
+}
+
+function create(skill) {
+  skills.push(skill);
+}
+
+function getOne(id) {
+  return skills[id];
+}
+
+function getAll() {
+  return skills;
+}
